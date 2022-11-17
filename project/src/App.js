7f5fba5df7085ca7145360286c72f3faa9d2fba7
davizacheu <zacheudavi@gmail.com>
2022-11-17 16:27:50 -0700

    added project files

---
+++ project/src/App.js
@@ -0,0 +1,21 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import About from "./pages/About";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="search" element={<Search />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<Navigate to="/" />}  />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
