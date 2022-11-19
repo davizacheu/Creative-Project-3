1c8561a9483b5ef7f7ea70fd09589c6b0f790547
davizacheu <zacheudavi@gmail.com>
2022-11-18 17:49:06 -0700

    github link adjusted

--- project/src/pages/Home.js
+++ project/src/pages/Home.js
@@ -2,7 +2,7 @@ import "./home.css";
 import Github from "./images/github.png"
 const Home = () => {
     return <div className="home">
-      <a href="https://github.com/davizacheu/Creative-Project-3"><img className="github" src={Github}></img></a>
+      <a href="https://github.com/davizacheu/Creative-Project-3/tree/davi"><img className="github" src={Github}></img></a>
     </div>
 
   };
