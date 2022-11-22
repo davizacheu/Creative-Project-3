52fbdc7c96eb8c8cf8a34ebf43c10e9f6f1f4ee7
Davi Fernandes <zacheudavi@gmail.com>
2022-11-22 21:16:18 +0000

    build done

--- project/src/pages/Home.js
+++ project/src/pages/Home.js
@@ -1,8 +1,10 @@
 import "./home.css";
-import Github from "./images/github.png"
 const Home = () => {
     return <div className="home">
-      <a href="https://github.com/davizacheu/Creative-Project-3/tree/davi"><img className="github" src={Github}></img></a>
+      <a href="https://github.com/davizacheu/Creative-Project-3/tree/davi">
+        <img className="github" src={require( "./images/github.png")} alt='link'></img>
+      </a>
+      
     </div>
 
   };
